45ca144b19de8f0510bc4454ce79049c058c4e9f
Tatyana <tanya.dyatchkova@gmail.com>
2022-12-09 22:58:01 +0300

    добавлена папка Task4_4

---
+++ Task4_4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 26 Hard необязательная. Напишите программу, которая принимает на вход целое или дробное число 
+// и выдает количество цифр и чисел
+// 452 -> 3
+// 82 -> 2
+// 9.012 -> 4
+Console.Write("Введите число: ");
+var num = Convert.ToDouble(Console.ReadLine());
+int i=0;
+while (num>0)
+{
+    num = num/10;
+    i++;
+}
+Console.WriteLine(num);
+Console.WriteLine(i);
