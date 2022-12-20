1e329f3d8a4306d28408ef41db9f1395b3c0c86e
Tatyana <tanya.dyatchkova@gmail.com>
2022-12-20 20:50:15 +0300

    коммит5

---
+++ Task8_4/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+int sideX = 3;
+int sideY = 3;
+int sideZ = 3;
+int max[Random];
+int[,,] arr3 = new int[];
+int[] arrShuffle = new int[maxRandom];
+FillArray(arr3,RandomShuffle(arrShuffle);
+
+int[,,] FillArray(int[,,] a)
+{
+    for (int i = 0; i < a.GetLength(0); i++)
+    {
+        for (int j = 0; j < a.GetLength(1); j++)
+        {
+            for (int k = 0; k < a.GetLength(2); k++)
+            {
+                a[i, j, k] = b[];
+                Console.Write($"{a[i, l, k]}{(i, j, k)} ");
+            } 
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int[RandomShuffle(int[] a)
+}
